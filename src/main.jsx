5f78514d5187b261e85248ecbef63a3324ce584f
Cleo Gao <celeo127@outlook.com>
2023-03-30 14:01:37 +0800

    added some css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import './index.css';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 //Scene
 const scene = new THREE.Scene();
@@ -31,13 +32,42 @@ const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 10
 camera.position.z = 20;
 scene.add(camera);
 
+
+
 //Canvas
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2)
 renderer.render(scene, camera);
 
+//controls
 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
+//resize
+window.addEventListener('resize', () => {
+  //update Sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  //update Camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+
+});
+
+const loop = () =>{
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
 
